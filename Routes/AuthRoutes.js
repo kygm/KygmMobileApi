8fbb7f0d222393de554bc82bb5f1f51bac29dc85
kygm <rickev20@gmail.com>
2023-11-09 10:00:06 -0600

    updated API version and added info route

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -31,36 +31,48 @@ require("../Models/UserModel");
 const User = mongoose.model('User');
 
 //debug -- not production code
-router.post("/", (req, res) => {
+router.post("/", (req, res) =>
+{
   return res.send("Dogget")
 });
 
-router.get("/ApiVersion", (req, res) => {
-  return res.send("1.0.0.1");
+router.get("/ApiVersion", (req, res) =>
+{
+  return res.send("1.0.1.0");
+});
+router.get("/Info", (req, res) =>
+{
+  return res.send("Developed by KYGM for CMSC2204");
 });
 
-router.post("/login", async (req, res) => {
+router.post("/login", async (req, res) =>
+{
   let { username, password } = req.body;
 
   let existingUser;
-  try {
+  try
+  {
     existingUser = await User.findOne({ Username: username });
   }
-  catch (err) {
+  catch (err)
+  {
     return res.status(400).send(err);
   }
-  if (!existingUser || existingUser.Password != password) {
+  if (!existingUser || existingUser.Password != password)
+  {
     return res.status(401).send("Verify credentials");
   }
   let token;
-  try {
+  try
+  {
     //Creating jwt token
     token = jwt.sign(
       { userId: existingUser.id, username: existingUser.Username },
       "842BD4ED27D41A82770BE39EB5A282CE37DD28A589F37D72D16C43AFF03379A8",
       { expiresIn: "1h" }
     );
-  } catch (err) {
+  } catch (err)
+  {
     return res.status(400).send(err);
   }
 
@@ -76,111 +88,141 @@ router.post("/login", async (req, res) => {
     });
 });
 
-router.post("/AddUser", async (req, res) => {
+router.post("/AddUser", async (req, res) =>
+{
   var token;
-  try {
+  try
+  {
     token = req.headers.authorization.split(' ')[1];
   }
   catch {
     return res.status(401).send("Auth problem")
   }
-  if (token && verifyToken(token)) {
-    try {
+  if (token && verifyToken(token))
+  {
+    try
+    {
       let user = {
         Username: req.body?.username,
         Password: req.body?.password,
         Email: req.body?.email,
         AuthLevel: req.body?.authLevel
       };
-      await User(user).save().then(() => {
+      await User(user).save().then(() =>
+      {
         return res.status(201).send("User created")
       });
     }
-    catch (error) {
+    catch (error)
+    {
       return res.status(500).send("An error occurred: " + error);
     }
   }
-  else {
+  else
+  {
     return res.status(401).send("Auth problem");
   }
 });
 
-router.get("/GetUsers", async (req, res) => {
+router.get("/GetUsers", async (req, res) =>
+{
   var token;
-  try {
+  try
+  {
     token = req.headers.authorization.split(' ')[1];
   }
   catch {
     return res.status(401).send("Auth problem")
   }
-  if (token && verifyToken(token)) {
-    try {
-      await User.find({}).then(users => {
+  if (token && verifyToken(token))
+  {
+    try
+    {
+      await User.find({}).then(users =>
+      {
         return res.status(200).json(users);
-      }, (res) => {
+      }, (res) =>
+      {
         console.log("rejected?" + res)
       });
     }
-    catch (error) {
+    catch (error)
+    {
       //console.log("Hit catch block with error: " + error)
       return res.status(500).send(`There was an error: ${error}`);
     }
   }
-  else {
+  else
+  {
     return res.status(401).send("Auth problem");
   }
 });
 
 //Delete
-router.post("/DeleteUserById", async (req, res) => {
+router.post("/DeleteUserById", async (req, res) =>
+{
   var token;
-  try {
+  try
+  {
     token = req.headers.authorization.split(' ')[1];
   }
   catch {
     return res.status(401).send("Auth problem")
   }
-  if (token && verifyToken(token)) {
-    try {
+  if (token && verifyToken(token))
+  {
+    try
+    {
       let user;
-      try {
+      try
+      {
         user = await User.findOne({ _id: req.body?.id });
       }
       catch {
         throw new Error("User lookup error");
       }
-      if (user) {
+      if (user)
+      {
         let result = await User.deleteOne({ _id: req.body.id });
-        if (result) {
+        if (result)
+        {
           return res.status(200).send(`${req.body.id} deleted`);
         }
-        else {
+        else
+        {
           return res.status(400).send(`Failed to delete user ${req.body.id}`);
         }
       }
-      else {
+      else
+      {
         return res.status(400).send("No user found");
       }
     }
-    catch (error) {
+    catch (error)
+    {
       return res.status(500).send(`There was an error: ${error}`);
     }
 
   }
-  else {
+  else
+  {
     return res.status(401).send("Auth problem");
   }
 });
 
 
 //this function is required in every route that uses authentication
-function verifyToken(token) {
-  try {
+function verifyToken(token)
+{
+  try
+  {
     result = jwt.verify(token, "842BD4ED27D41A82770BE39EB5A282CE37DD28A589F37D72D16C43AFF03379A8");
-    if (result == undefined) {
+    if (result == undefined)
+    {
       return false;
     }
-    else {
+    else
+    {
       return true;
     }
   }
