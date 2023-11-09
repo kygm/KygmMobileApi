ad13bf6688924d7d16e7ad3d9c86e9e9ca47e47b
kygm <rickev20@gmail.com>
2023-11-09 09:54:22 -0600

    added deployment logic (auto port set)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const mongoose = require('mongoose');
 const jwt = require("jsonwebtoken");
 mongoose.set('strictQuery', false);
-const PORT = 1250;
+const PORT = process.env.PORT || 1250;
 const dbUrl = "mongodb+srv://admin:Password1@cluster.qtabs.mongodb.net/DemoServices?retryWrites=true&w=majority";
 mongoose.connect(dbUrl,
   {
@@ -17,10 +17,12 @@ const version = "1.0.2";
 
 const db = mongoose.connection;
 
-db.on('error', () => {
+db.on('error', () =>
+{
   console.error.bind(console, 'connection error: ');
 });
-db.once('open', () => {
+db.once('open', () =>
+{
   console.log('MongoDB Connected');
 });
 
@@ -36,46 +38,57 @@ app.use('/Auth', authRoutes)
  * using the following pattern: "^\w{2,20}Routes.js$" in ./Routes
  */
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
   return res.status(200).json(version);
 });
 
-app.get("/AuthTest", (req, res) => {
+app.get("/AuthTest", (req, res) =>
+{
   var token;
-  try {
+  try
+  {
     token = req.headers.authorization.split(' ')[1];
   }
   catch {
     return res.status(401).send("Auth problem");
   }
-  if (token && verifyToken(token)) {
+  if (token && verifyToken(token))
+  {
     //Authorized user operations here
     return res.status(200).send("Auth success");
   }
-  else {
+  else
+  {
     return res.status(401).send("Auth problem");
   }
 });
 
 //These final items need to be at end of route definitions in index
-app.get('*', (req, res) => {
+app.get('*', (req, res) =>
+{
   res.status(404).send(
     "Route not found, check method type, params, and spelling"
   );
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () =>
+{
   console.log(`Server Started on port ${PORT}`);
 });
 
 //this function is required in every route that uses authentication
-function verifyToken(token) {
-  try {
+function verifyToken(token)
+{
+  try
+  {
     result = jwt.verify(token, "842BD4ED27D41A82770BE39EB5A282CE37DD28A589F37D72D16C43AFF03379A8");
-    if (result == undefined) {
+    if (result == undefined)
+    {
       return false;
     }
-    else {
+    else
+    {
       return true;
     }
   }
